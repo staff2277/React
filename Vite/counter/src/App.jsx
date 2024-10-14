355fe72023914381c9a2e89a4a63471de42c194e
staff227 <wnfynn@gmail.com>
2024-10-14 21:05:50 +0000

    react

--- Vite/counter/src/App.jsx
+++ Vite/counter/src/App.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>App
+      <h1>Hello, Vite!</h1>
+      <p>This is a basic Vite React app.</p>
+    </div>
   )
 }
 
