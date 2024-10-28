e2a0aeea5581b971ae3869c120de19c207cba758
staff227 <wnfynn@gmail.com>
2024-10-28 21:07:17 +0000

    rendered

--- Vite/SandBox/components/Input.jsx
+++ Vite/SandBox/components/Input.jsx
@@ -4,35 +4,61 @@ import unchecked from "/unchecked.svg";
 import close from "/close.svg";
 
 const Input = () => {
-  let input = document.getElementById("input");
-
-  const [todo, setTodo] = useState("");
-
-
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
+  const [checkStatus, setCheckStatus] = useState(true);
 
   function addTodo() {
-    setTodo(input.value)
-    console.log(input.value);
+    if (input) {
+      setTodos(() => [...todos, input]);
+      setInput("");
+    }
   }
+
+  const [checkedStatusImg, setCheckedStatusImg] = useState(unchecked);
+
+  
+  let todoArray = todos.map((todo, index) => {
+    function deleteTask(){
+      setTodos(todos.filter((_,i)=>i !== index))
+    }
+
+    function toggleCheckedStatus(){
+      setCheckStatus((prevstate)=> !prevstate)
+      if(checkStatus){
+        setCheckedStatusImg(() => checked)
+      }else{
+        setCheckedStatusImg(() => unchecked)
+      }
+    }
+    return(    
+    <div
+      key={index}
+      className="flex justify-between border-2 items-center py-3"
+    >
+      <img key={index} src={checkedStatusImg} alt="check" className="w-[50px]" onClick={toggleCheckedStatus}/>
+      <span>{todo}</span>
+      <img src={close} alt="close" className="w-[50px]" onClick={deleteTask}/>
+    </div>
+  )})
 
   return (
     <div className="min-w-[70%]">
       <div className="flex justify-center border-2">
         <input
           type="text"
+          value={input}
+          onChange={(change) => {
+            setInput(() => change.target.value);
+          }}
           placeholder="Type your task"
           className="border-2  px-[1rem] py-[12px] text-[1.4rem] rounded-2xl w-full"
         />
         <button className="border-2 text-[2.5rem] px-[30px]" onClick={addTodo}>
           +
         </button>
-        
       </div>
-      <div className="flex justify-between border-2 items-center py-3">
-          <img src={checked} alt="check" className="w-[50px]" />
-          <span>asaa</span>
-          <img src={close} alt="close" className="w-[50px]" />
-        </div>
+      {todoArray}
     </div>
   );
 };
---
+++ Vite/Xora/src/App.jsx
@@ -0,0 +1,11 @@
+import Header from './Sections/Header'
+
+const App = () => {
+  return (
+    <main className='overflow-hidden'>
+      <Header/>
+    </main>
+  )
+}
+
+export default App
